967e5dacb7814b552c9109cd3706ec0c53799540
neethuramesan <neethuramesan1@gmail.com>
2022-07-13 22:15:57 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'home.html')
+
+def group(request):
+    return render(request, 'groups.html')
+
+def branch(request):
+    return render(request, 'branch.html')
+
+def ledger(request):
+    return render(request, 'ledger.html')
+
+def primary(request):
+    return render(request, 'primarycost.html')
+
+def costcat(request):
+    return render(request, 'costcat.html')
+
+def costcentr(request):
+    return render(request, 'costcentr.html')
+
+def voucher(request):
+    return render(request, 'voucher.html')
+
+def vouchpage(request):
+    return render(request, 'vouchpage.html')
+
+def create(request):
+    return render(request,'company_create.html')
+
+def features(request):
+    return render(request,'features.html')
+
+
+
+
